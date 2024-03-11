30d0fffe8318ed7b24d3280b31f1cf44b950c492
Matthew <12hughesm@gmail.com>
2024-03-11 16:18:12 +0000

    Created newUser and validateUser classes

--- src/main/java/library/NewUser.java
+++ src/main/java/library/NewUser.java
@@ -1,4 +1,49 @@
 package library;
 
+import file.utils.JsonFileWriter;
+import utils.InputManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NewUser {
+    JsonFileWriter jsonFileWriter = new JsonFileWriter();
+    InputManager inputManager = new InputManager();
+
+    List<User> users;
+
+    public NewUser(List<User> users) {
+        this.users = users;
+    }
+
+    public void registerNewUser(){
+        System.out.println("Please enter a username");
+        String username = inputManager.getStringInput();
+        
+        if(usernameExists(username)){
+            System.out.println("Username is taken");
+            registerNewUser();
+            return;
+        }
+
+        System.out.println("Please enter a password");
+        String password = inputManager.getStringInput();
+
+        createNewUser(username, password);
+        System.out.println("New user created created");
+    }
+
+    private void createNewUser(String username, String password) {
+        User user = new User(username, password, new ArrayList<>(), false);
+        users.add(user);
+    }
+
+    private boolean usernameExists(String username) {
+        for(User user : users){
+            if (user.getUsername().equals(username)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
---
+++ src/main/java/library/validateUser.java
@@ -0,0 +1,14 @@
+package library;
+
+import java.util.List;
+
+public class validateUser {
+    public static boolean isUserValid(String username, String password, List<User> users){
+        for(User user : users){
+            if(user.getUsername().equals(username) && user.getPassword().equals(password)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
